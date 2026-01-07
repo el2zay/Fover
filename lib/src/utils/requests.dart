7660b6039e1560289861d97f646c9f2be6dfbdfa
Elie <79168733+el2zay@users.noreply.github.com>
2026-01-07 19:03:33 +0100

    Update requests.dart

--- lib/src/utils/requests.dart
+++ lib/src/utils/requests.dart
@@ -2,7 +2,10 @@ import 'dart:io';
 
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:freebox/freebox.dart';
+import 'package:freebox_photos/main.dart';
 import 'package:get_storage/get_storage.dart';
+import 'dart:developer';
+
 
 final box = GetStorage();
 
@@ -26,4 +29,21 @@ Future signUp(context) async {
   }
 
   return register;
+}
+
+// Permet d'afficher tous les dossiers
+Future fetchDir() async {
+  var directories = await client?.fetch(url: "v15/fs/ls/L0ZyZWVib3g=");
+
+  log(directories.toString());
+}
+
+// Permet d'afficher le dossier sélectionné
+Future fetchPhotosDir() async {
+  var directories = await client?.fetch(url: "v15/fs/ls/L0ZyZWVib3gvVGVzdA==");
+  List<dynamic> entries = directories?['result']['entries'] ?? [];
+
+  var filesOnly = entries.where((e) => e['name'] != '.' && e['name'] != '..' && (e['mimetype'].contains('image/') || e['mimetype'].contains('video/')));
+  
+  log(filesOnly.toString());
 }
