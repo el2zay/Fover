2ddbfbf93b19789b9ac8ed90ba26512aed496ec1
Elie <79168733+el2zay@users.noreply.github.com>
2026-03-01 15:41:16 +0100

    New dialog widget in liquid glass

---
+++ lib/src/widgets/dialog.dart
@@ -0,0 +1,62 @@
+import 'package:cupertino_native_better/cupertino_native.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDialog extends StatelessWidget {
+  final String content;
+  final Function onTap;
+  const MyDialog({super.key, required this.content, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 160,
+        margin: const EdgeInsets.symmetric(horizontal: 40),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: LiquidGlassContainer(
+          config: LiquidGlassConfig(
+            shape: CNGlassEffectShape.rect,
+            cornerRadius: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsetsGeometry.all(20),
+                child: Material(
+                  color: Colors.transparent,
+                  child: Text(
+                    content,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Color.fromARGB(235, 255, 255, 255)),
+                    textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Divider(thickness: 0.5, height: 1, color: Colors.white24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      child: Text("Cancel", style: TextStyle(fontSize: 16, color: CupertinoColors.activeBlue)),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }
+                    ),
+                    TextButton(
+                      child: Text("Delete", style: TextStyle(fontSize: 16, color: CupertinoColors.destructiveRed)),
+                      onPressed: () {}
+                    ),
+                  ],
+                )
+              ]
+            )
+          )
+        ),
+    );
+  }
+}
