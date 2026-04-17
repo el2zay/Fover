8172402a3061691f6ac6dba398054a87036fb1f0
Elie <79168733+el2zay@users.noreply.github.com>
2026-04-17 23:45:53 +0200

    Blurredappbar that adapts to the theme

--- lib/src/widgets/blurred_app_bar.dart
+++ lib/src/widgets/blurred_app_bar.dart
@@ -1,10 +1,5 @@
 import 'dart:ui';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-
-
 
 class BlurredAppBar extends StatefulWidget implements PreferredSizeWidget {
   final String title;
@@ -57,68 +52,71 @@ class _BlurredAppBarState extends State<BlurredAppBar> {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     final textColor = (isAtTop && !isDark) ? Colors.black : Colors.white;
 
-    return AppBar(
-      clipBehavior: Clip.none,
-      elevation: 0,
-      centerTitle: false,
-      flexibleSpace: ClipRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 2, sigmaY: 0),
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: isDark ? [
-                  Colors.black.withAlpha(150),
-                  Colors.black.withAlpha(150),
-                  Colors.black.withAlpha(25),
-                ] : [
-                  Colors.white.withAlpha(25),
-                  Colors.white.withAlpha(25),
-                ]
+    return OrientationBuilder(builder: (context, orientation) {
+      return AppBar(
+        clipBehavior: Clip.none,
+        elevation: 0,
+        centerTitle: false,
+        flexibleSpace: orientation == Orientation.landscape ? ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 0),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: isDark ? [
+                    Colors.black.withAlpha(150),
+                    Colors.black.withAlpha(150),
+                    Colors.black.withAlpha(25),
+                  ] : [
+                    Colors.white.withAlpha(25),
+                    Colors.white.withAlpha(25),
+                  ]
+                ),
               ),
             ),
           ),
-        ),
-      ),
-      titleSpacing: 10,
-      title: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-            AnimatedDefaultTextStyle(
-              duration: Duration(milliseconds: 200),
-              style: TextStyle(
-                color: widget.scrollController != null ? textColor : Theme.of(context).primaryColor,
-                fontSize: 36,
-                fontWeight: FontWeight.bold
-              ),
-              child: Text(
-                widget.title, 
+        ) : SizedBox(),
+        titleSpacing: 10,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (orientation == Orientation.landscape) SizedBox(height: 10),
+              AnimatedDefaultTextStyle(
+                duration: Duration(milliseconds: 200),
                 style: TextStyle(
-                  color: widget.isAlbum ? Colors.black : null,
-                  fontSize: widget.isAlbum ? 20 : 36,
+                  color: widget.scrollController != null ? textColor : Theme.of(context).primaryColor,
+                  fontSize: 36,
                   fontWeight: FontWeight.bold
-                )
+                ),
+                child: Text(
+                  widget.title, 
+                  style: TextStyle(
+                    color: widget.isAlbum ? Colors.black : null,
+                    fontSize: widget.isAlbum ? 20 : 36,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
               ),
-            ),
-          if (widget.subtitle != null)
-            AnimatedDefaultTextStyle(
-              duration: Duration(milliseconds: 200),
-              style: TextStyle(
-                color: widget.isAlbum 
-                  ? Theme.of(context).primaryColor
-                  : textColor.withAlpha(200),
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),
-              child: Text(widget.subtitle!), 
-            )
-        ],
-      ),
-      backgroundColor: Colors.transparent,
-      actions: widget.actions,
-    );  
+            if (widget.subtitle != null)
+              AnimatedDefaultTextStyle(
+                duration: Duration(milliseconds: 200),
+                style: TextStyle(
+                  color: widget.isAlbum 
+                    ? Theme.of(context).primaryColor
+                    : textColor.withAlpha(200),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+                child: Text(widget.subtitle!), 
+              )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        actions: widget.actions,
+      );  
+    });
   }
 }
