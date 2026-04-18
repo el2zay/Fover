b6fd7fc88605a85298a45fd1c9fbc003c0a2cba3
Elie <79168733+el2zay@users.noreply.github.com>
2026-04-18 12:47:49 +0200

    Orientation locked for phones

--- lib/pages/onboarding/first.dart
+++ lib/pages/onboarding/first.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fover/pages/onboarding/copyparty/login.dart';
 import 'package:fover/pages/onboarding/freebox/login.dart';
+import 'package:fover/src/utils/common_utils.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  void initState() {
+    super.initState();
+    if (!isTablet) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown
+      ]);
+    }
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,66 +77,65 @@ class FirstPage extends StatelessWidget {
             ),
             Positioned(
               bottom: 30,
-              left: 0,
-              right: 0,
+              left: 20,
+              right: 20,
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: 
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white10,
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.white30),
-                ),
-                constraints: BoxConstraints(
-                  maxWidth: 500
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.075,
-                  vertical: 24,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "Fover connects to your home server and turns it into your personal cloud, without the monthly bill",
-                      style: TextStyle(fontSize: 16.5, fontWeight: FontWeight.w500, color: Colors.white70),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.black,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        onPressed: () => Navigator.push(context, CupertinoPageRoute(builder: (_) => CopypartyLoginPage())),
-                        child: const Text("Start with your server", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white10,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.white30),
+                  ),
+                  constraints: BoxConstraints(
+                    maxWidth: 500
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.075,
+                    vertical: 24,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Fover connects to your home server and turns it into your personal cloud, without the monthly bill",
+                        style: TextStyle(fontSize: 16.5, fontWeight: FontWeight.w500, color: Colors.white70),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
                           ),
+                          onPressed: () => Navigator.push(context, CupertinoPageRoute(builder: (_) => CopypartyLoginPage())),
+                          child: const Text("Start with your server", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
                         ),
-                        onPressed: () => Navigator.push(context, CupertinoPageRoute(builder: (_) => FreeboxLoginPage())),
-                        child: const Text("Start with Freebox", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          onPressed: () => Navigator.push(context, CupertinoPageRoute(builder: (_) => FreeboxLoginPage())),
+                          child: const Text("Start with Freebox", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-            ),
               ),
             ),
             const SizedBox(height: 30),
