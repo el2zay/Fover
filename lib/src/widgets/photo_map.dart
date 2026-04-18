e215ebbe6061de89e31b8fc3437112c343076bda
Elie <79168733+el2zay@users.noreply.github.com>
2026-04-19 01:15:10 +0200

    Show all photo locations

--- lib/src/widgets/photo_map.dart
+++ lib/src/widgets/photo_map.dart
@@ -2,11 +2,12 @@ import 'package:apple_maps_flutter/apple_maps_flutter.dart';
 import 'package:cupertino_native_better/cupertino_native.dart' show CNSymbol;
 import 'package:flutter/material.dart';
 import 'package:fover/src/models/photo_entry.dart';
+import 'package:fover/src/services/photo_store.dart';
 import 'package:fover/src/widgets/button.dart';
 import 'package:geolocator/geolocator.dart';
 
 class PhotoMap extends StatefulWidget {
-  final PhotoEntry photo;
+  final PhotoEntry? photo;
   final bool fullscreen;
   const PhotoMap({super.key, required this.photo, this.fullscreen = false});
 
@@ -17,13 +18,14 @@ class PhotoMap extends StatefulWidget {
 class _PhotoMapState extends State<PhotoMap> {
   AppleMapController? _mapController;
 
-  Future<void> myLocation() async {
-    final position = await Geolocator.getCurrentPosition();
-    _mapController?.animateCamera(
-      CameraUpdate.newLatLng(
+  LatLng? myLocation() {
+    Geolocator.getCurrentPosition().then((position) {
+      _mapController?.animateCamera(CameraUpdate.newLatLng(
         LatLng(position.latitude, position.longitude)
-      )
-    );
+      ));
+      return LatLng(position.latitude, position.longitude);
+    });
+    return null;
   }
 
   @override
@@ -34,16 +36,23 @@ class _PhotoMapState extends State<PhotoMap> {
           myLocationEnabled: true,
           myLocationButtonEnabled: false,
           onMapCreated: (controller) => _mapController = controller,
-          annotations: { 
+          annotations: widget.photo != null ? {
             Annotation(
-              annotationId: AnnotationId(widget.photo.name),
-              position: LatLng(widget.photo.latitude ?? 0, widget.photo.longitude ?? 0)
+              annotationId: AnnotationId(widget.photo?.path ?? ""),
+              position: LatLng(widget.photo!.latitude!, widget.photo!.longitude!)
             ),
-          },
-          // onMapCreated: _onMapCreated,
+          } : PhotoStore.getGeotagged().map((e) => Annotation(
+              annotationId: AnnotationId(e.path),
+              position: LatLng(e.latitude!, e.longitude!)
+            )).toSet(),
+
           initialCameraPosition: CameraPosition(
             zoom: 13,
-            target: LatLng(widget.photo.latitude ?? 0, widget.photo.longitude ?? 0),
+            target: widget.photo != null 
+              ? LatLng(widget.photo!.latitude!, widget.photo!.longitude!)
+              // : myLocation() ?? const LatLng(48.8584, 2.2945)
+              : myLocation() ?? const LatLng(0,0)
+
           ),
         ),
         // TODO rendre responsive
