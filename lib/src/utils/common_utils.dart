c09c72d3c02ace62d5b02c66234d64df46148cfd
Elie <79168733+el2zay@users.noreply.github.com>
2026-05-24 15:45:05 +0200

    Check if offline mode is enabled

--- lib/src/utils/common_utils.dart
+++ lib/src/utils/common_utils.dart
@@ -72,6 +72,8 @@ String formatSize(int size) {
 }
 
 Future<bool> hasInternet() async {
+  if (box.get("offlineMode") == true) return false;
+  
   try {
     final result = await InternetAddress.lookup('example.com');
     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
