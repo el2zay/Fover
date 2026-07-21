da58138504aea5e7b21745a0725ea80c2e273a62
Elie <79168733+el2zay@users.noreply.github.com>
2026-07-21 11:44:34 +0200

    Offline Support for Videos

--- lib/src/utils/video_controller.dart
+++ lib/src/utils/video_controller.dart
@@ -1,30 +1,47 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:fover/main.dart';
 import 'package:fover/src/services/copyparty_service.dart';
+import 'package:fover/src/services/download.dart';
+import 'package:fover/src/services/photo_store.dart';
 import 'package:fover/src/utils/common_utils.dart';
 import 'package:video_player/video_player.dart';
 
 Future<VideoPlayerController?> buildVideoController(String encodedPath) async {
+  VideoPlayerController controller;
   final decodedPath = Uri.decodeFull(encodedPath);
   final lastSlash = decodedPath.lastIndexOf('/');
   final dir = lastSlash >= 0 ? decodedPath.substring(0, lastSlash + 1) : '';
   final filename = lastSlash >= 0 ? decodedPath.substring(lastSlash + 1) : decodedPath;
 
-  final cleanedName = cleanName(filename, removeExtension: true);
-  final hlsPath = '$dir.hls/$cleanedName/master.m3u8';
+  if (DownloadService.isDownloaded(encodedPath)) {
+    final localPath = PhotoStore.get(encodedPath)?.localPath;
+    if (localPath == null) return null;
 
-  final hlsUri = Uri.parse(CopypartyService.baseUrl).resolve(
-    'photos/${hlsPath.split('/').map(Uri.encodeComponent).join('/')}',
-  );
+    final controller = VideoPlayerController.file(
+      File(localPath),
+    );
 
-  final originalUri = Uri.parse(CopypartyService.baseUrl).resolve(
-    'photos/${decodedPath.split('/').map(Uri.encodeComponent).join('/')}',
-  );
+    try {
+      await controller.initialize();
+      return controller;
+    } catch (_) {
+      await controller.dispose();
+      return null;
+    }
+  } else if (detectBackend() == ServerBackend.copyparty) {
+    final cleanedName = cleanName(filename, removeExtension: true);
+    final hlsPath = '$dir.hls/$cleanedName/master.m3u8';
 
-  VideoPlayerController controller;
+    final hlsUri = Uri.parse(CopypartyService.baseUrl).resolve(
+      'photos/${hlsPath.split('/').map(Uri.encodeComponent).join('/')}',
+    );
 
-  if (detectBackend() == ServerBackend.copyparty) {
+    final originalUri = Uri.parse(CopypartyService.baseUrl).resolve(
+      'photos/${decodedPath.split('/').map(Uri.encodeComponent).join('/')}',
+    );
+
     final hlsExists = await urlExists(
       hlsUri.toString(),
       headers: {'Authorization': 'Basic ${CopypartyService.credentials}'}
@@ -38,7 +55,6 @@ Future<VideoPlayerController?> buildVideoController(String encodedPath) async {
     );
 
     log('hlsUri: $hlsUri\n hlsExists: $hlsExists originalUri: $originalUri');
-
   } else {
     controller = VideoPlayerController.networkUrl(
       Uri.parse('https://${box.get('apiDomain')}:${box.get('httpsPort')}/api/v1/5dl$encodedPath'),
